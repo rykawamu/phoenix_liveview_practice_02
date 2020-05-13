28ce5420f6fa20aa288e0d9b61aefaadb20e4df0
rykawamu <ryu2.endless.waltz@gmail.com>
2020-05-14 01:22:06 +0900

    hiyokoプロジェクトを--liveオプションなし（LiveView非対応）で作成

---
+++ nolive/hiyoko/config/config.exs
@@ -0,0 +1,28 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :hiyoko, HiyokoWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "5iYo6aqGK0MVQFhUg+/mD14xhnpIcpbwwC9QsoqTM6FMVFGrcIe+u4w7ZYTY1sQp",
+  render_errors: [view: HiyokoWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Hiyoko.PubSub,
+  live_view: [signing_salt: "+HzAB7mP"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
