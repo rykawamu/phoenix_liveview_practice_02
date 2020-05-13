18577c63bbec1fa0abc19e53e90bfdc6dbaa6825
rykawamu <ryu2.endless.waltz@gmail.com>
2020-05-14 01:02:24 +0900

    WebAPI経由による検索画面を作成

--- live/hiyoko/lib/hiyoko_web/router.ex
+++ live/hiyoko/lib/hiyoko_web/router.ex
@@ -18,6 +18,8 @@ defmodule HiyokoWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+
+    live "/connpass", Connpass
   end
 
   # Other scopes may use custom stacks.
--- live/hiyoko/mix.exs
+++ live/hiyoko/mix.exs
@@ -43,6 +43,7 @@ defmodule Hiyoko.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:smallex, "~> 0.2.3" },
       {:plug_cowboy, "~> 2.0"}
     ]
   end
